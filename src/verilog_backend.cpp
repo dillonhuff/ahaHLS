5432fc1bb0073ce2ae3b877626748096a8366881
Dillon Huff <dillonhuff@gmail.com>
2018-10-13 14:21:16 -0700

    Working on verilog backend

--- src/verilog_backend.cpp
+++ src/verilog_backend.cpp
@@ -1,19 +1,36 @@
 #include "verilog_backend.h"
 
 #include <fstream>
+#include <llvm/IR/Instructions.h>
 
 using namespace llvm;
 using namespace std;
 
 namespace DHLS {
 
+  int clog2(const int val) {
+    return ceil(log2(val));
+  }
+
   class Port {
   public:
     bool isInput;
     int width;
     std::string name;
+
+    std::string toString() {
+      return string(isInput ? "input" : "output") + " [" + to_string(width - 1) + ":0] " + name;
+    }
   };
 
+  Port inputPort(const int width, const std::string& name) {
+    return {true, width, name};
+  }
+
+  Port outputPort(const int width, const std::string& name) {
+    return {false, width, name};
+  }
+  
   std::string commaListString(const std::vector<std::string>& strings) {
     string res = "";
     for (int i = 0; i < (int) strings.size(); i++) {
@@ -23,6 +40,53 @@ namespace DHLS {
       }
     }
     return res;
+  }
+
+  std::vector<Port> getPorts(const STG& stg) {
+    vector<Port> pts = {inputPort(1, "clk"), inputPort(1, "rst")};
+    int numReadPorts = 0;
+    int numWritePorts = 0;
+
+    for (auto state : stg.opStates) {
+      int numReadsInState = 0;
+      int numWritesInState = 0;
+      for (auto gInstr : state.second) {
+        Instruction* i = gInstr.instruction;
+
+        if (StoreInst::classof(i)) {
+          numWritesInState++;
+        }
+
+        if (LoadInst::classof(i)) {
+          numReadsInState++;
+        }
+
+      }
+
+      if (numReadsInState > numReadPorts) {
+        numReadPorts = numReadsInState;
+      }
+
+      if (numWritesInState > numWritePorts) {
+        numWritePorts = numWritesInState;
+      }
+    }
+
+
+    // TODO: Accomodate different width reads / writes
+    int width = 32;    
+    for (int i = 0; i < numReadPorts; i++) {
+      pts.push_back(inputPort(width, "rdata_" + to_string(i)));
+      pts.push_back(outputPort(clog2(width), "raddr_" + to_string(i)));
+    }
+
+    for (int i = 0; i < numWritePorts; i++) {
+      pts.push_back(outputPort(width, "wdata_" + to_string(i)));
+      pts.push_back(outputPort(clog2(width), "waddr_" + to_string(i)));
+      pts.push_back(outputPort(width, "wen_" + to_string(i)));
+    }
+
+    return pts;
   }
 
   // What are the components of this verilog?
@@ -39,7 +103,13 @@ namespace DHLS {
 
     string fn = f->getName();
 
-    vector<string> portStrings = {"input clk", "input rst"};
+    vector<Port> allPorts = getPorts(stg);
+
+    vector<string> portStrings;
+    for (auto pt : allPorts) {
+      portStrings.push_back(pt.toString());
+    }
+
     ofstream out(fn + ".v");
     out << "module " << fn << "(" + commaListString(portStrings) + ");" << endl;
     out << "endmodule" << endl;
