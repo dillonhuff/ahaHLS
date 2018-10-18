47bf9d871356847ddfd33892757c1f9a86243970
Dillon Huff <dillonhuff@gmail.com>
2018-10-17 17:55:14 -0700

    Creating if else tb

--- builtins.v
+++ builtins.v
@@ -8,5 +8,9 @@ module add(input [31:0]  in0, input [31:0] in1, output [31:0] out);
    assign out = in0 + in1;
 endmodule
 
+module eq(input [31:0]  in0, input [31:0] in1, output [0:0] out);
+   assign out = in0 == in1;
+endmodule
+
 module ret();
 endmodule
