d8699b4851924280fc48839e8b4244b0b52e0225
Dillon Huff <dillonhuff@gmail.com>
2018-11-26 15:13:12 -0600

    Generated blur code that uses temporary variables gives the wrong answer? Not sure what the problem is there.

--- test/ll_files/blur_lb.c
+++ test/ll_files/blur_lb.c
@@ -4,9 +4,14 @@
 void blur_lb(int* __restrict a, int* __restrict b) {
   int a_near = a[0];
   int a_mid = a[1];
-  int a_far = a[2];
+  int a_far;
 
   for (int i = 1; i < A_LEN - 1; i++) {
+    a_far = a[i + 1];
+    
     b[i - 1] = a_near + a_mid + a_far;
+    a_near = a_mid;
+    a_mid = a_far;
+
   }
 }
