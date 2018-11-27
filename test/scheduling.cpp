1e2bc27e67750ff75ecaaf4d92d36d85da2170fa
Dillon Huff <dillonhuff@gmail.com>
2018-11-27 14:46:33 -0800

    I suspect the linebuffered blur failure has to do with the way that the system picks between the tmp stored value of a variable and the live version of a variable, but I dont know that

--- test/scheduling.cpp
+++ test/scheduling.cpp
@@ -780,14 +780,14 @@ namespace DHLS {
     // Idea: Check in assertions that write data never has an x when write
     // enable is high
     VerilogDebugInfo info;
-    // info.wiresToWatch.push_back({false, 32, "global_state_dbg"});
-    // info.wiresToWatch.push_back({false, 32, "wdata_temp_reg_dbg"});
+    info.wiresToWatch.push_back({false, 32, "global_state_dbg"});
+    info.wiresToWatch.push_back({false, 32, "wdata_temp_reg_dbg"});
 
-    // info.debugAssigns.push_back({"global_state_dbg", "global_state"});
+    info.debugAssigns.push_back({"global_state_dbg", "global_state"});
 
-    // info.debugWires.push_back({true, 32, "num_clocks_after_reset"});
+    info.debugWires.push_back({true, 32, "num_clocks_after_reset"});
 
-    // addAlwaysBlock({"clk"}, "if (rst) begin num_clocks_after_reset <= 0; end else begin num_clocks_after_reset <= num_clocks_after_reset + 1; end", info);
+    addAlwaysBlock({"clk"}, "if (rst) begin num_clocks_after_reset <= 0; end else begin num_clocks_after_reset <= num_clocks_after_reset + 1; end", info);
 
     // addAlwaysBlock({"clk"}, "if (num_clocks_after_reset == 2) begin $display(\"waddr_0_reg == %d\", waddr_0_reg); end", info);
     // addAlwaysBlock({"clk"}, "if (num_clocks_after_reset == 2) begin $display(\"wdata_0_reg == %d\", wdata_0_reg); end", info);
@@ -798,10 +798,11 @@ namespace DHLS {
     // addWirePrintoutIf("num_clocks_after_reset == 1", "add_in1_11", info);
     // addWirePrintoutIf("num_clocks_after_reset == 1", "phi_out_5", info);
     // addWirePrintoutIf("num_clocks_after_reset == 1", "add_tmp_7", info);
-    // addWirePrintoutIf("num_clocks_after_reset == 1", "last_BB_reg", info);
+    addWirePrintoutIf("num_clocks_after_reset == 2", "wdata_0_reg", info);
 
-    // addAssert("num_clocks_after_reset !== 2 || waddr_0_reg === 8", info);
-    // addAssert("num_clocks_after_reset !== 2 || wen_0_reg === 1", info);
+    addAssert("num_clocks_after_reset !== 2 || waddr_0_reg === 8", info);
+    addAssert("num_clocks_after_reset !== 2 || wen_0_reg === 1", info);
+    addAssert("num_clocks_after_reset !== 2 || wdata_0_reg === 3", info);
 
     emitVerilog(f, graph, layout, info);
 
@@ -825,7 +826,7 @@ namespace DHLS {
     tb.name = "blur_lb";
     emitVerilogTestBench(tb, layout);
 
-    //REQUIRE(runIVerilogTB("blur_lb"));
+    REQUIRE(runIVerilogTB("blur_lb"));
     
   }
   
