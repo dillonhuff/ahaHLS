8269aa96c4750569f0e962d7d3b8c7ba70f99919
Dillon Huff <dillonhuff@gmail.com>
2018-11-27 18:51:42 -0800

    Need to track whether each state transition is actually taking the code to a new basic block

--- test/scheduling.cpp
+++ test/scheduling.cpp
@@ -795,6 +795,7 @@ namespace DHLS {
     addWirePrintout("phi_in0_13", info);
     addWirePrintout("phi_in1_13", info);    
     addWirePrintout("phi_out_13", info);
+    addWirePrintout("last_BB_reg", info);
     // Aasdf
 
     addAssert("num_clocks_after_reset !== 2 || waddr_0_reg === 8", info);
