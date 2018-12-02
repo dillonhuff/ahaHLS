c2ba92812966963e80ca0d5fa886efbbe9f770de
Dillon Huff <dillonhuff@gmail.com>
2018-12-02 12:14:16 -0800

    Instantiated axi module

--- stalled_single_store_axi_tb.v
+++ stalled_single_store_axi_tb.v
@@ -11,7 +11,39 @@ module test();
    reg clk;
    reg rst;
    wire valid;
+
+   parameter ADDR_WIDTH = 5;
+   parameter DATA_WIDTH = 32;
+   parameter STRB_WIDTH = (DATA_WIDTH/8);   
+
+   // --- AXI Vars   
+   // Not used   
+   reg [2:0]            s_axil_awprot;
+   // Not used
+   reg [2:0]            s_axil_arprot;
+
+   wire [ADDR_WIDTH-1:0] s_axil_awaddr;
+
+   wire [DATA_WIDTH-1:0] s_axil_wdata;
+   wire [STRB_WIDTH-1:0] s_axil_wstrb;
+   wire                  s_axil_wvalid;
+   wire                  s_axil_bready;
+
+   wire [ADDR_WIDTH-1:0] s_axil_araddr;
+   wire                 s_axil_arvalid;
+   wire                   s_axil_rready;
+
    
+   wire                 s_axil_awready;
+   wire                 s_axil_wready;
+   wire [1:0]           s_axil_bresp;
+   wire                 s_axil_bvalid;
+
+
+   wire                 s_axil_arready;
+   wire [DATA_WIDTH-1:0] s_axil_rdata;
+   wire [1:0]            s_axil_rresp;
+   wire                  s_axil_rvalid;
 
    // Depth 16, width 32 RAM
    wire [4:0] raddr;
@@ -105,7 +137,39 @@ module test();
 
    end
 
-   axil_ram axiram(.clk(clk), .rst(rst));
+   axil_ram #(.DATA_WIDTH(32), .ADDR_WIDTH(5))
+   ram(.clk(clk),
+       .rst(rst),
+
+
+       .s_axil_awprot(s_axil_awprot),
+       .s_axil_arprot(s_axil_arprot),
+
+       .s_axil_awaddr(s_axil_awaddr),       
+       .s_axil_awvalid(s_axil_awvalid),
+
+       .s_axil_awready(s_axil_awready),
+       .s_axil_wdata(s_axil_wdata),
+
+       .s_axil_wstrb(s_axil_wstrb),
+       .s_axil_wvalid(s_axil_wvalid),
+
+       .s_axil_wready(s_axil_wready),
+       .s_axil_bresp(s_axil_bresp),
+
+       .s_axil_bvalid(s_axil_bvalid),
+
+       .s_axil_bready(s_axil_bready),
+       .s_axil_araddr(s_axil_araddr),
+
+       .s_axil_arvalid(s_axil_arvalid),
+       .s_axil_arready(s_axil_arready),
+
+       .s_axil_rdata(s_axil_rdata),
+       .s_axil_rresp(s_axil_rresp),
+
+       .s_axil_rvalid(s_axil_rvalid),
+       .s_axil_rready(s_axil_rready));
    
    stalled_single_store_axi ss(.clk(clk), .rst(rst), .valid(valid), .waddr_0(waddr), .wdata_0(wdata), .wen_0(wen), .raddr_0(raddr), .rdata_0(rdata), .global_stall(global_stall));
 
