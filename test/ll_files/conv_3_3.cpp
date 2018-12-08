b033f8f77f7a872cec7a82aa8305951d2a70a591
Dillon Huff <dillonhuff@gmail.com>
2018-12-08 15:11:32 -0800

    Writing conv

--- test/ll_files/conv_3_3.cpp
+++ test/ll_files/conv_3_3.cpp
@@ -1,6 +1,15 @@
-#define IN_DIM 8
+#include <iostream>
 
-#define ARR(a, i, j) (a)[(i)*IN_DIM + (j)]
+using namespace std;
+
+#define IN_DIM 8
+#define OUT_DIM (IN_DIM - 2)
+#define KERNEL_DIM 3
+
+#define ARR(a, i, j, stride) (a)[(i)*(stride) + (j)]
+#define GET_A(a, i, j) ARR((a), (i), (j), IN_DIM)
+#define GET_K(a, i, j) ARR((a), (i), (j), KERNEL_DIM)
+#define GET_O(a, i, j) ARR((a), (i), (j), OUT_DIM)
 
 void conv_3_3(const int* __restrict in,
               const int* __restrict kernel,
@@ -8,19 +17,50 @@ void conv_3_3(const int* __restrict in,
 
   for (int i = 1; i < IN_DIM - 1; i++) {
     for (int j = 1; j < IN_DIM - 1; j++) {
-      int top_row = ARR(kernel, 0, 0)*ARR(in, i - 1, j - 1) +
-        ARR(kernel, 0, 1)*ARR(in, i - 1, j) +
-        ARR(kernel, 0, 2)*ARR(in, i - 1, j + 1);
+      int top_row = GET_K(kernel, 0, 0)*GET_A(in, i - 1, j - 1) +
+        GET_K(kernel, 0, 1)*GET_A(in, i - 1, j) +
+        GET_K(kernel, 0, 2)*GET_A(in, i - 1, j + 1);
 
-      int mid_row = ARR(kernel, 1, 0)*ARR(in, i, j - 1) +
-        ARR(kernel, 1, 1)*ARR(in, i, j) +
-        ARR(kernel, 1, 2)*ARR(in, i, j + 1);
+      int mid_row = GET_K(kernel, 1, 0)*GET_A(in, i, j - 1) +
+        GET_K(kernel, 1, 1)*GET_A(in, i, j) +
+        GET_K(kernel, 1, 2)*GET_A(in, i, j + 1);
 
-      int low_row = ARR(kernel, 2, 0)*ARR(in, i + 1, j - 1) +
-        ARR(kernel, 2, 1)*ARR(in, i + 1, j) +
-        ARR(kernel, 2, 2)*ARR(in, i + 1, j + 1);
+      int low_row = GET_K(kernel, 2, 0)*GET_A(in, i + 1, j - 1) +
+        GET_K(kernel, 2, 1)*GET_A(in, i + 1, j) +
+        GET_K(kernel, 2, 2)*GET_A(in, i + 1, j + 1);
 
-      ARR(out, i, j) = top_row + mid_row + low_row;
+      GET_O(out, i - 1, j - 1) = top_row + mid_row + low_row;
     }
   }
+}
+
+int main() {
+  int* in = (int*) malloc(sizeof(int)*IN_DIM*IN_DIM);
+  int* kernel = (int*) malloc(sizeof(int)*(KERNEL_DIM*KERNEL_DIM));
+  for (int i = 0; i < KERNEL_DIM; i++) {
+    for (int j = 0; j < KERNEL_DIM; j++) {
+      GET_K(kernel, i, j) = 1;
+    }
+  }
+
+  for (int i = 0; i < IN_DIM; i++) {
+    for (int j = 0; j < IN_DIM; j++) {
+      GET_A(in, i, j) = 1;
+    }
+  }
+  
+  const int out_size = (IN_DIM - 2)*(IN_DIM - 2);
+  int* out = (int*) malloc(sizeof(int)*out_size);
+
+  conv_3_3(in, kernel, out);
+
+  for (int i = 0; i < IN_DIM - 2; i++) {
+    for (int j = 0; j < IN_DIM - 2; j++) {
+      cout << GET_O(out, i, j) << " ";
+    }
+    cout << endl;
+  }
+  
+  free(in);
+  free(kernel);
 }
