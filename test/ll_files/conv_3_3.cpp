63b86b6a5fe1f26d5c9d088571fe3b0f6ad4ec5d
Dillon Huff <dillonhuff@gmail.com>
2018-12-08 12:32:38 -0800

    Playing with larger convolution

---
+++ test/ll_files/conv_3_3.cpp
@@ -0,0 +1,26 @@
+#define IN_DIM 8
+
+#define ARR(a, i, j) (a)[(i)*IN_DIM + (j)]
+
+void conv_3_3(int* __restrict in,
+              int* __restrict kernel,
+              int* __restrict out) {
+
+  for (int i = 1; i < IN_DIM - 1; i++) {
+    for (int j = 1; j < IN_DIM - 1; j++) {
+      int top_row = ARR(kernel, 0, 0)*ARR(in, i - 1, j - 1) +
+        ARR(kernel, 0, 1)*ARR(in, i - 1, j) +
+        ARR(kernel, 0, 2)*ARR(in, i - 1, j + 1);
+
+      int mid_row = ARR(kernel, 1, 0)*ARR(in, i, j - 1) +
+        ARR(kernel, 1, 1)*ARR(in, i, j) +
+        ARR(kernel, 1, 2)*ARR(in, i, j + 1);
+
+      int low_row = ARR(kernel, 2, 0)*ARR(in, i + 1, j - 1) +
+        ARR(kernel, 2, 1)*ARR(in, i + 1, j) +
+        ARR(kernel, 2, 2)*ARR(in, i + 1, j + 1);
+
+      ARR(out, i, j) = top_row + mid_row + low_row;
+    }
+  }
+}
