680e4c0827d696dba22d30f4f186d9316357014b
Dillon Huff <dillonhuff@gmail.com>
2018-12-19 13:30:20 -0800

    Removed polly header file

--- src/scheduling.h
+++ src/scheduling.h
@@ -3,7 +3,7 @@
 #include "algorithm.h"
 #include "utils.h"
 
-#include <polly/Canonicalization.h>
+//#include <polly/Canonicalization.h>
 #include <llvm/IR/Module.h>
 #include <llvm/IR/Instructions.h>
 #include <llvm/Support/raw_ostream.h>
