431e1e234b3b75db66857fdfa472c994622f1e28
Dillon Huff <dillonhuff@gmail.com>
2019-01-01 23:30:39 -0800

    Factoring out test_utils

---
+++ test/test_utils.cpp
@@ -0,0 +1,87 @@
+#include "test_utils.h"
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+using namespace llvm;
+
+namespace DHLS {
+
+
+  int createLLFile(const std::string& moduleName) {
+    return system(("clang -O1 -c -S -emit-llvm " + moduleName + ".c -o " + moduleName + ".ll").c_str());
+  }
+
+  std::unique_ptr<Module> loadLLFile(LLVMContext& Context,
+                                     SMDiagnostic& Err,
+                                     const std::string& name) {
+    string modFile = "./test/ll_files/" + name + ".ll";
+    cout << "Parsing ir file " << modFile << endl;
+    std::unique_ptr<Module> Mod(parseIRFile(modFile, Err, Context));
+    if (!Mod) {
+      outs() << "Error: No mod\n";
+      assert(false);
+    }
+
+    return Mod;
+  }
+  
+  std::unique_ptr<Module> loadModule(LLVMContext& Context,
+                                     SMDiagnostic& Err,
+                                     const std::string& name) {
+    int res = createLLFile("./test/ll_files/" + name);
+    assert(res == 0);
+    
+    return loadLLFile(Context, Err, name);
+  }
+
+  bool runCmd(const std::string& cmd) {
+    cout << "Running command: " << cmd << endl;
+    bool res = system(cmd.c_str());
+    return res == 0;
+  }
+
+  bool runIVerilogTB(const std::string& moduleName) {
+    string mainName = moduleName + "_tb.v";
+    string modFile = moduleName + ".v";
+
+    string genCmd = "iverilog -g2005 -o " + moduleName + " " + mainName + " " + modFile + " RAM.v RAM2.v RAM3.v axil_ram.v delay.v builtins.v";
+    bool compiled = runCmd(genCmd);
+
+    if (!compiled) {
+      return false;
+    }
+
+    string resFile = moduleName + "_tb_result.txt";
+    string exeCmd = "./" + moduleName + " > " + resFile;
+    bool ran = runCmd(exeCmd);
+
+    assert(ran);
+
+    ifstream res(resFile);
+    std::string str((std::istreambuf_iterator<char>(res)),
+                    std::istreambuf_iterator<char>());
+
+    cout << "str = " << str << endl;
+    
+    //runCmd("rm -f " + resFile);
+
+    reverse(begin(str), end(str));
+    string lastLine;
+
+    for (int i = 1; i < (int) str.size(); i++) {
+      if (str[i] == '\n') {
+        break;
+      }
+
+      lastLine += str[i];
+    }
+
+    reverse(begin(lastLine), end(lastLine));
+
+    cout << "Lastline = " << lastLine << endl;
+    return lastLine == "Passed";
+  }
+  
+}
---
+++ test/test_utils.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/SourceMgr.h>
+
+#include <llvm/IR/Module.h>
+
+namespace DHLS {
+
+  int createLLFile(const std::string& moduleName);
+
+  std::unique_ptr<llvm::Module> loadLLFile(llvm::LLVMContext& Context,
+                                           llvm::SMDiagnostic& Err,
+                                           const std::string& name);
+
+  std::unique_ptr<llvm::Module> loadModule(llvm::LLVMContext& Context,
+                                           llvm::SMDiagnostic& Err,
+                                           const std::string& name);
+
+  bool runCmd(const std::string& cmd);
+
+  bool runIVerilogTB(const std::string& moduleName);
+}
