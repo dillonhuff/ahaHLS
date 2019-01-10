86a45c413db7ed7b38871aeab985032b6715cfaa
Dillon Huff <dillonhuff@gmail.com>
2019-01-10 15:14:49 -0800

    Fifo write takes 2 cycles?

--- fifo.v
+++ fifo.v
@@ -32,51 +32,12 @@ module test();
       write_valid0 = 1;
 
       `POSEDGE      
+      `POSEDGE
 
-      in_data0 = 2;
-      write_valid0 = 1;
-
-      #1 `assert(write_ready0, 1'd1)
-      #1 `assert(read_ready0, 1'd1)
-
-
-
-      in_data0 = 3;
-      write_valid0 = 1;
-
-      #1 `assert(write_ready0, 1'd1)
-      #1 `assert(read_ready0, 1'd1)
-
-      `POSEDGE                  
-
-      #1 in_data0 = 4;
-      #1 write_valid0 = 1;
-
-      #1 `assert(write_ready0, 1'd1)
-      #1 `assert(read_ready0, 1'd1)
-
-      `POSEDGE                  
-
-      #1 in_data0 = 5;
-      #1 write_valid0 = 1;
-
-      #1 `assert(write_ready0, 1'd1)
-      #1 `assert(read_ready0, 1'd1)
-        
-      `POSEDGE                          
-
-      #1 write_valid0 = 0;
-
-      `POSEDGE                        
+      write_valid0 = 0;
 
       #1 `assert(write_ready0, 1'd0)
       #1 `assert(read_ready0, 1'd1)
-
-      `POSEDGE                        
-      `POSEDGE                        
-      `POSEDGE                        
-      `POSEDGE                              
-      `POSEDGE                                    
       
       #1 $display("Passed");
 
@@ -87,6 +48,6 @@ module test();
       $display("out_data0 = %d", out_data0);
    end
 
-   fifo #(.WIDTH(32), .DEPTH(4)) in(.clk(clk), .rst(rst), .read_valid(read_valid0), .read_ready(read_ready0), .write_ready(write_ready0), .write_valid(write_valid0), .out_data(out_data0), .in_data(in_data0));
+   fifo #(.WIDTH(32), .DEPTH(1)) in(.clk(clk), .rst(rst), .read_valid(read_valid0), .read_ready(read_ready0), .write_ready(write_ready0), .write_valid(write_valid0), .out_data(out_data0), .in_data(in_data0));
    
 endmodule
