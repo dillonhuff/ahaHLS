5c1fdc42ec8a68813c2cf36e5b7cd8ba5a2e5f2c
Dillon Huff <dillonhuff@gmail.com>
2019-01-10 20:11:12 -0800

    Creating fifo use in a loop

--- test/scheduling.cpp
+++ test/scheduling.cpp
@@ -2490,5 +2490,73 @@ namespace DHLS {
     REQUIRE(runIVerilogTest(mName, eName));
     
   }
+
+  TEST_CASE("Reading and writing FIFOs") {
+    LLVMContext context;
+    setGlobalLLVMContext(&context);
+
+    int width = 16;
+    auto iStr = to_string(width);
+
+    StructType* tp = StructType::create(context, "builtin_fifo_" + iStr);
+
+    auto mod = llvm::make_unique<Module>("fifo use in a loop", context);
+
+    vector<Type*> readArgs = {tp->getPointerTo()};
+    Function* readFifo =
+      mkFunc(readArgs, intType(width), "builtin_read_fifo_" + iStr, mod.get());
+
+    vector<Type*> writeArgs = {tp->getPointerTo(), intType(width)};
+    Function* writeFifo =
+      mkFunc(readArgs, "builtin_write_fifo_" + iStr, mod.get());
+    
+    std::vector<Type *> inputs{tp->getPointerTo(),
+        tp->getPointerTo()};
+    Function* f = mkFunc(inputs, "fifo_in_loop", mod.get());
+
+    auto entryBlock = mkBB("entry_block", f);
+    auto exitBlock = mkBB("exit_block", f);
+
+    ConstantInt* loopBound = mkInt("10", width);
+    ConstantInt* three = mkInt("3", width);
+
+    auto bodyF = [f, readFifo, writeFifo, width](IRBuilder<>& builder, Value* i) {
+
+      auto val = builder.CreateCall(readFifo, {getArg(f, 0)});
+      auto p0 = builder.CreateAdd(mkInt(2, width), val);
+      auto sum = builder.CreateAdd(p0, val);
+      builder.CreateCall(writeFifo, {sum, getArg(f, 1)});
+    };
+    auto loopBlock = sivLoop(f, entryBlock, exitBlock, three, loopBound, bodyF);
+
+    IRBuilder<> entryBuilder(entryBlock);        
+    entryBuilder.CreateBr(loopBlock);
+
+    IRBuilder<> exitBuilder(exitBlock);
+    exitBuilder.CreateRet(nullptr);
+    
+    cout << "LLVM function" << endl;
+    cout << valueString(f) << endl;
+
+    HardwareConstraints hcs = standardConstraints();
+    hcs.setCount(ADD_OP, 1);
+    Schedule s = scheduleFunction(f, hcs);
+
+    STG graph = buildSTG(s, f);
+
+    cout << "STG is " << endl;
+    graph.print(cout);
+    
+    map<llvm::Value*, int> layout = {};
+    ArchOptions options;
+    auto arch = buildMicroArchitecture(f, graph, layout, options, hcs);
+
+    VerilogDebugInfo info;
+    addNoXChecks(arch, info);
+
+    emitVerilog(f, arch, info);
+
+    REQUIRE(runIVerilogTB("fifo_in_loop"));
+  }
   
 }
