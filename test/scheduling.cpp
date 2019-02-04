1b7191fabab67578284c107a4feac6a1b09b2304
Dillon Huff <dillonhuff@gmail.com>
2019-02-03 21:31:46 -0800

    Pulled out writeRam0

--- test/scheduling.cpp
+++ test/scheduling.cpp
@@ -4231,6 +4231,41 @@ namespace DHLS {
 
   }
 
+  void implementRAMWrite0(Function* ramWrite0, InterfaceFunctions& interfaces) {
+    int addrWidth = getValueBitWidth(getArg(ramWrite0, 1));
+    int width = getValueBitWidth(getArg(ramWrite0, 2));
+    auto sramTp = ramWrite0->getReturnType();
+
+    ExecutionConstraints& exec = interfaces.getConstraints(ramWrite0);
+    auto waddr0F = writePort("waddr_0", addrWidth, sramTp);
+    auto wdata0F = writePort("wdata_0", width, sramTp);
+    auto wen0F = writePort("wen_0", 1, sramTp);
+
+    auto sram = getArg(ramWrite0, 0);
+    auto addr = getArg(ramWrite0, 1);
+    auto data = getArg(ramWrite0, 2);
+
+    auto bb = mkBB("entry_block", ramWrite0);
+    IRBuilder<> eb(bb);
+    auto setAddr = eb.CreateCall(waddr0F, {sram, addr});
+    auto setData = eb.CreateCall(wdata0F, {sram, data});
+    auto setEn1 = eb.CreateCall(wen0F, {sram, mkInt(1, 1)});
+    auto setEn0 = eb.CreateCall(wen0F, {sram, mkInt(0, 1)});
+    eb.CreateRet(nullptr);
+
+    exec.add(instrStart(setAddr) == instrStart(setData));
+    exec.add(instrStart(setAddr) == instrStart(setEn1));
+
+    exec.add(instrEnd(setEn1) + 1 == instrStart(setEn0));
+
+    //addDataConstraints(ramWrite0, exec);
+
+    cout << "-- Constraints on write function" << endl;
+    for (auto c : exec.constraints) {
+      cout << tab(1) << *c << endl;
+    }
+  }
+  
   TEST_CASE("Creating memory interface functions") {
     LLVMContext context;
     setGlobalLLVMContext(&context);
@@ -4253,36 +4288,7 @@ namespace DHLS {
 
     Function* ramWrite0 = mkFunc({sramTp, intType(addrWidth), intType(width)}, voidType(), "write0");
     interfaces.addFunction(ramWrite0);
-    {
-      auto& exec = interfaces.getConstraints(ramWrite0);
-      auto waddr0F = writePort("waddr_0", addrWidth, sramTp);
-      auto wdata0F = writePort("wdata_0", width, sramTp);
-      auto wen0F = writePort("wen_0", 1, sramTp);
-
-      auto sram = getArg(ramWrite0, 0);
-      auto addr = getArg(ramWrite0, 1);
-      auto data = getArg(ramWrite0, 2);
-
-      auto bb = mkBB("entry_block", ramWrite0);
-      IRBuilder<> eb(bb);
-      auto setAddr = eb.CreateCall(waddr0F, {sram, addr});
-      auto setData = eb.CreateCall(wdata0F, {sram, data});
-      auto setEn1 = eb.CreateCall(wen0F, {sram, mkInt(1, 1)});
-      auto setEn0 = eb.CreateCall(wen0F, {sram, mkInt(0, 1)});
-      eb.CreateRet(nullptr);
-
-      exec.add(instrStart(setAddr) == instrStart(setData));
-      exec.add(instrStart(setAddr) == instrStart(setEn1));
-
-      exec.add(instrEnd(setEn1) + 1 == instrStart(setEn0));
-
-      //addDataConstraints(ramWrite0, exec);
-
-      cout << "-- Constraints on write function" << endl;
-      for (auto c : exec.constraints) {
-        cout << tab(1) << *c << endl;
-      }
-    }
+    implementRAMWrite0(ramWrite0, interfaces);
   
 
     FunctionType *tp =
