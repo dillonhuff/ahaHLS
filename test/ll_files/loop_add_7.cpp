26cfb8942ee804b339ee545fb4ad20b1b1cc176c
Dillon Huff <dillonhuff@gmail.com>
2019-02-17 02:41:05 -0800

    Removed some comments

--- test/ll_files/loop_add_7.cpp
+++ test/ll_files/loop_add_7.cpp
@@ -3,9 +3,8 @@
 #define a 0
 #define b 10
 
-void loop_add_7(RAM<int, 16>* ram) { //int* __restrict a, int* __restrict b) {
+void loop_add_7(RAM<int, 16>* ram) {
   for (int i = 0; i < 10; i++) {
     ram->write(a + i, ram->read(b + i) + 7);
-    //a[i] = b[i] + 7;
   }
 }
