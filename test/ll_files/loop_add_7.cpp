ef0545e4011c1e8329855d1b657635da6c744620
Dillon Huff <dillonhuff@gmail.com>
2019-02-17 02:36:23 -0800

    Converting loop_add_7

---
+++ test/ll_files/loop_add_7.cpp
@@ -0,0 +1,9 @@
+#define a 0
+#define b 10
+
+void loop_add_7(RAM<int, 16>* ram) { //int* __restrict a, int* __restrict b) {
+  for (int i = 0; i < 10; i++) {
+    ram->write(a + i, ram->read(b + i) + 7);
+    //a[i] = b[i] + 7;
+  }
+}
