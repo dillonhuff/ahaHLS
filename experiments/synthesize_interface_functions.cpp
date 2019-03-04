765ff57cc03714022f4317916ad955628f7a7221
Dillon Huff <dillonhuff@gmail.com>
2019-03-03 19:48:50 -0800

    Added interface function cpp

---
+++ experiments/synthesize_interface_functions.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+
+#include "verilog_backend.h"
+#include "llvm_codegen.h"
+
+#include <llvm/IRReader/IRReader.h>
+#include <llvm/Support/SourceMgr.h>
+#include <llvm/IR/Module.h>
+
+using namespace DHLS;
+using namespace llvm;
+using namespace std;
+
+int main() {
+  LLVMContext context;
+  SMDiagnostic err;
+  setGlobalLLVMContext(&context);
+
+  auto mod = llvm::make_unique<Module>("ram_write", context);
+  setGlobalLLVMModule(mod.get());
+
+  HardwareConstraints hcs = standardConstraints();
+  hcs.typeSpecs[string("SRAM_32_16")] =
+    [](StructType* tp) { return ramSpec(32, 16); };
+  InterfaceFunctions interfaces;
+  Function* ramRead = ramLoadFunction(32, 16);
+  interfaces.addFunction(ramRead);
+  implementRAMRead0(ramRead,
+                    interfaces.getConstraints(ramRead));
+
+  Function* ramWrite = ramStoreFunction(32, 16);
+  interfaces.addFunction(ramWrite);
+  implementRAMWrite0(ramWrite,
+                     interfaces.getConstraints(ramWrite));
+
+  Schedule s = scheduleInterface(ramWrite, hcs, interfaces);
+
+  STG graph = buildSTG(s, ramWrite);
+
+  cout << "STG Is" << endl;
+  graph.print(cout);
+
+  map<llvm::Value*, int> layout;
+  auto arch = buildMicroArchitecture(ramWrite, graph, layout, hcs);
+
+  VerilogDebugInfo info;
+  emitVerilog("ram_write", graph, hcs, info);
+
+}
