adf881c2fc3bce375b921656d283999091e088b6
Dillon Huff <dillonhuff@gmail.com>
2019-03-04 15:24:02 -0800

    Added fpu add to repo

--- experiments/synthesize_interface_functions.cpp
+++ experiments/synthesize_interface_functions.cpp
@@ -121,5 +121,141 @@ int main() {
     }
     
   }
+
+  {
+    int width = 32;
+    auto iStr = to_string(width);
+
+    InterfaceFunctions interfaces;
+    auto fpuType =
+      llvm::StructType::create(getGlobalLLVMContext(),
+                               "builtin_fadd");
+    
+    Function* fadd =
+      mkFunc({fpuType->getPointerTo(), intType(width), intType(width)}, intType(width), "jd_fadd");
+    interfaces.addFunction(fadd);
+    {
+      ExecutionConstraints& exec = interfaces.getConstraints(fadd);
+      auto entryBlock = mkBB("entry_block", fadd);
+      IRBuilder<> b(entryBlock);
+      auto f = fadd;
+
+      auto fpu = getArg(f, 0);
+      auto a = getArg(f, 1);
+      auto b0 = getArg(f, 2);
+
+      // Interface with floating point adder
+      auto writeRst = writePort("rst", 1, fpuType);
+      auto writeA = writePort("input_a", 32, fpuType);
+      auto writeAStb = writePort("input_a_stb", 1, fpuType);
+
+      auto writeB = writePort("input_b", 32, fpuType);
+      auto writeBStb = writePort("input_b_stb", 1, fpuType);
+      auto stall = stallFunction();
+
+      auto readAAck = readPort("input_a_ack", 1, fpuType);
+      auto readBAck = readPort("input_b_ack", 1, fpuType);
+      auto readZStb = readPort("output_z_stb", 1, fpuType);
+
+      auto rst0 = b.CreateCall(writeRst, {fpu, mkInt(1, 1)});
+      // Wait until next cycle
+      auto rst1 = b.CreateCall(writeRst, {fpu, mkInt(0, 1)});
+
+      exec.endsBeforeStarts(rst0, rst1);
+
+      auto wA = b.CreateCall(writeA, {fpu, a});
+      auto wAStb = b.CreateCall(writeAStb, {fpu, mkInt(1, 1)});
+
+      exec.add(instrEnd(rst1) < instrStart(wA));
+      //exec.startSameTime(rst1, wA);    
+      exec.startSameTime(wA, wAStb);    
+
+      // Wait for input_a_ack == 1, and then wait 1 more cycle
+      auto aAck = b.CreateCall(readAAck, {fpu});
+      auto stallUntilAAck = b.CreateCall(stall, {aAck});
+
+      auto wAStb0 = b.CreateCall(writeAStb, {fpu, mkInt(0, 1)});
+      auto wB = b.CreateCall(writeB, {fpu, b0});
+      auto wBStb = b.CreateCall(writeBStb, {fpu, mkInt(1, 1)});
+
+      auto bAck = b.CreateCall(readBAck, {fpu});
+      auto stallUntilBAck = b.CreateCall(stall, {bAck});
+    
+      // Wait one or two cycles?
+      auto wBStb0 = b.CreateCall(writeBStb, {fpu, mkInt(0, 1)});
+
+      // Wait at least one cycle after input_b_stb == 1, for output_z_stb == 1
+      auto zStb = b.CreateCall(readZStb, {fpu});
+      auto stallUntilZStb = b.CreateCall(stall, {zStb});
+      auto val = b.CreateCall(readPort("output_z", 32, fpuType), {fpu});
+
+      b.CreateRet(val);
+      
+      // A / B stall
+      exec.addConstraint(instrStart(aAck) == instrEnd(wAStb));
+      exec.startsBeforeStarts(aAck, wAStb0);
+      exec.addConstraint(instrStart(stallUntilAAck) == instrEnd(aAck));
+
+      
+      exec.startSameTime(wA, wAStb);
+
+      exec.addConstraint(instrStart(bAck) == instrEnd(wBStb));
+      exec.endsBeforeStarts(bAck, wBStb0);
+      exec.addConstraint(instrStart(stallUntilBAck) == instrEnd(bAck));
+      exec.startSameTime(wB, wBStb);    
+
+      // Wait for A to be written before writing b
+      exec.addConstraint(instrEnd(aAck) < instrStart(wB));
+
+      // Wait for b to be acknowledged before reading Z
+      exec.addConstraint(instrEnd(bAck) < instrStart(val));
+
+      exec.startSameTime(val, zStb);
+
+      exec.startSameTime(stallUntilZStb, zStb);
+      addDataConstraints(fadd, exec);
+    }
+
+    HardwareConstraints hcs = standardConstraints();
+    map<string, Port> adderPorts = {
+      {"input_a", inputPort(width, "input_a")},
+      {"input_a_stb", inputPort(1, "input_a_stb")},
+      {"input_b", inputPort(width, "input_b")},
+      {"input_b_stb", inputPort(1, "input_b_stb")},
+      {"rst", inputPort(1, "rst")},
+
+      {"output_z", outputPort(width, "output_z")},
+      {"input_a_ack", outputPort(1, "input_a_ack")},
+      {"input_b_ack", outputPort(1, "input_b_ack")},
+      {"output_z_stb", outputPort(1, "output_z_stb")}
+    };
+
+    map<string, int> defaults = {
+      {"input_a", 0},
+      {"input_a_stb", 0},
+      {"input_b", 0},
+      {"input_b_stb", 0},
+      {"rst", 0}
+    };
+
+    //
+    //setAllAllocaMemTypes(hcs, f, registerSpec(width));
+    hcs.typeSpecs[fpuType->getName()] =
+      [adderPorts, defaults](StructType* fp) { return ModuleSpec({}, "adder", adderPorts, defaults); };
+    hcs.typeSpecs["builtin_fifo_32"] =
+      [width](StructType* tp) { return wireSpec(width); };
+    
+    Schedule s = scheduleInterface(fadd, hcs, interfaces, toPipeline, interfaces.getConstraints(fadd));
+    STG graph = buildSTG(s, fadd);
+
+    cout << "STG Is" << endl;
+    graph.print(cout);
+
+    map<llvm::Value*, int> layout;
+    auto arch = buildMicroArchitecture(fadd, graph, layout, hcs);
+
+    VerilogDebugInfo info;
+    emitVerilog("fpu_add", graph, hcs, info);
+  }
   
 }
---
+++ fpu_add.v
@@ -0,0 +1,265 @@
+module fpu_add_inner(input [0:0] clk, input [0:0] rst, output [31:0] arg_0_input_a, output [0:0] arg_0_input_a_stb, output [31:0] arg_0_input_b, output [0:0] arg_0_input_b_stb, output [0:0] arg_0_rst, input [0:0] arg_0_input_a_ack, input [0:0] arg_0_input_b_ack, input [31:0] arg_0_output_z, input [0:0] arg_0_output_z_stb, output [0:0] valid, input [31:0] arg_1, input [31:0] arg_2);
+
+	reg [31:0] arg_0_input_a_reg;
+	reg [0:0] arg_0_input_a_stb_reg;
+	reg [31:0] arg_0_input_b_reg;
+	reg [0:0] arg_0_input_b_stb_reg;
+	reg [0:0] arg_0_rst_reg;
+	reg [0:0] valid_reg;
+
+	assign arg_0_input_a = arg_0_input_a_reg;
+	assign arg_0_input_a_stb = arg_0_input_a_stb_reg;
+	assign arg_0_input_b = arg_0_input_b_reg;
+	assign arg_0_input_b_stb = arg_0_input_b_stb_reg;
+	assign arg_0_rst = arg_0_rst_reg;
+	assign valid = valid_reg;
+
+	// Start debug wires and ports
+
+	initial begin
+	end
+
+
+
+
+
+	// End debug wires and ports
+
+	// Start Functional Units
+	add call_5();
+
+	add call_10();
+
+	add call_13();
+
+	// End Functional Units
+
+	// Start instruction result storage
+	// End instruction result storage
+
+	// Start pipeline variables
+	// End pipeline variables
+
+	reg [31:0] global_state;
+	reg [31:0] last_BB_reg;
+	// Start pipeline reset block
+	always @(posedge clk) begin
+		if (rst) begin
+		end
+	end
+	// End pipeline reset block
+
+	// Start pipeline valid chain block
+	always @(posedge clk) begin
+
+		if (!rst) begin
+		end
+	end
+	// End pipeline valid chain block
+
+	always @(posedge clk) begin
+	end
+	// Start pipeline initiation block
+	always @(posedge clk) begin
+	end
+	// End pipeline initiation block
+
+	always @(posedge clk) begin
+		if (rst) begin
+			last_BB_reg <= 0;
+		end else begin
+			if ((global_state == 0)) begin
+			end
+			if ((global_state == 1)) begin
+			end
+			if ((global_state == 2)) begin
+			end
+			if ((global_state == 3)) begin
+			end
+			if ((global_state == 4)) begin
+					last_BB_reg <= 0;
+			end
+		end
+	end
+
+	always @(posedge clk) begin
+		if (rst) begin
+			global_state <= 0;
+		end else begin
+			if ((global_state == 0)) begin 
+				// Next state transition logic
+				// Condition = True
+
+				if (1) begin
+					global_state <= 1;
+				end
+				// Store data computed at the stage
+			end
+			if ((global_state == 1)) begin 
+				// Next state transition logic
+				// Condition = True
+
+				if (1) begin
+					global_state <= 2;
+				end
+				// Store data computed at the stage
+			end
+			if ((global_state == 2)) begin 
+				// Next state transition logic
+				// Condition = True
+
+				if (1) begin
+				if (arg_0_input_a_ack) begin 
+					global_state <= 3;
+				end
+				end
+				// Store data computed at the stage
+			end
+			if ((global_state == 3)) begin 
+				// Next state transition logic
+				// Condition = True
+
+				if (1) begin
+				if (arg_0_input_b_ack) begin 
+					global_state <= 4;
+				end
+				end
+				// Store data computed at the stage
+			end
+			if ((global_state == 4)) begin 
+				// Next state transition logic
+				// Condition = True
+
+				if (1) begin
+				if (arg_0_output_z_stb) begin 
+					global_state <= 4;
+				end
+				end
+				// Store data computed at the stage
+			end
+		end
+	end
+
+
+	// Start pipeline instruction code
+	// Start pipeline stages
+	// End pipeline instruction code
+
+	always @(*) begin
+		if ((global_state == 0)) begin 
+				//   call void @builtin_write_port_rst(%builtin_fadd* %arg_0, i1 true)
+				arg_0_rst_reg = -(1'd1);
+			arg_0_input_a_reg = 0;
+			arg_0_input_a_stb_reg = 0;
+			arg_0_input_b_reg = 0;
+			arg_0_input_b_stb_reg = 0;
+		end else 		if ((global_state == 1)) begin 
+				//   call void @builtin_write_port_rst(%builtin_fadd* %arg_0, i1 false)
+				arg_0_rst_reg = (1'd0);
+			arg_0_input_a_reg = 0;
+			arg_0_input_a_stb_reg = 0;
+			arg_0_input_b_reg = 0;
+			arg_0_input_b_stb_reg = 0;
+		end else 		if ((global_state == 2)) begin 
+				//   call void @builtin_write_port_input_a(%builtin_fadd* %arg_0, i32 %arg_1)
+				if (arg_0_input_a_ack) begin
+				arg_0_input_a_reg = arg_1;
+				end
+				//   call void @builtin_write_port_input_a_stb(%builtin_fadd* %arg_0, i1 true)
+				if (arg_0_input_a_ack) begin
+				arg_0_input_a_stb_reg = -(1'd1);
+				end
+				//   %0 = call i1 @builtin_read_port_input_a_ack(%builtin_fadd* %arg_0)
+				if (arg_0_input_a_ack) begin
+				end
+			arg_0_input_b_reg = 0;
+			arg_0_input_b_stb_reg = 0;
+			arg_0_rst_reg = 0;
+		end else 		if ((global_state == 3)) begin 
+				//   call void @builtin_write_port_input_a_stb(%builtin_fadd* %arg_0, i1 false)
+				if (arg_0_input_b_ack) begin
+				arg_0_input_a_stb_reg = (1'd0);
+				end
+				//   call void @builtin_write_port_input_b(%builtin_fadd* %arg_0, i32 %arg_2)
+				if (arg_0_input_b_ack) begin
+				arg_0_input_b_reg = arg_2;
+				end
+				//   call void @builtin_write_port_input_b_stb(%builtin_fadd* %arg_0, i1 true)
+				if (arg_0_input_b_ack) begin
+				arg_0_input_b_stb_reg = -(1'd1);
+				end
+				//   %1 = call i1 @builtin_read_port_input_b_ack(%builtin_fadd* %arg_0)
+				if (arg_0_input_b_ack) begin
+				end
+			arg_0_input_a_reg = 0;
+			arg_0_rst_reg = 0;
+		end else 		if ((global_state == 4)) begin 
+				//   call void @builtin_write_port_input_b_stb(%builtin_fadd* %arg_0, i1 false)
+				if (arg_0_output_z_stb) begin
+				arg_0_input_b_stb_reg = (1'd0);
+				end
+				//   %2 = call i1 @builtin_read_port_output_z_stb(%builtin_fadd* %arg_0)
+				if (arg_0_output_z_stb) begin
+				end
+				//   %3 = call i32 @builtin_read_port_output_z(%builtin_fadd* %arg_0)
+				if (arg_0_output_z_stb) begin
+				end
+			arg_0_input_a_reg = 0;
+			arg_0_input_a_stb_reg = 0;
+			arg_0_input_b_reg = 0;
+			arg_0_rst_reg = 0;
+		end else begin 
+			// Default values
+				arg_0_input_a_reg = 0;
+				arg_0_input_a_stb_reg = 0;
+				arg_0_input_b_reg = 0;
+				arg_0_input_b_stb_reg = 0;
+				arg_0_rst_reg = 0;
+		end
+	end
+	always @(*) begin
+		if ((global_state == 2)) begin 
+				//   call void @builtin_stall(i1 %0)
+		end else begin 
+			// Default values
+		end
+	end
+	always @(*) begin
+		if ((global_state == 3)) begin 
+				//   call void @builtin_stall(i1 %1)
+		end else begin 
+			// Default values
+		end
+	end
+	always @(*) begin
+		if ((global_state == 4)) begin 
+				//   call void @builtin_stall(i1 %2)
+		end else begin 
+			// Default values
+		end
+	end
+	always @(*) begin
+		if ((global_state == 4)) begin 
+				//   ret i32 %3
+				if (arg_0_output_z_stb) begin
+				valid_reg = 1;
+				end
+		end else begin 
+			// Default values
+				valid_reg = 0;
+		end
+	end
+endmodule
+
+module fpu_add(input [0:0] clk, input [0:0] rst, output [31:0] arg_0_input_a, output [0:0] arg_0_input_a_stb, output [31:0] arg_0_input_b, output [0:0] arg_0_input_b_stb, output [0:0] arg_0_rst, input [0:0] arg_0_input_a_ack, input [0:0] arg_0_input_b_ack, input [31:0] arg_0_output_z, input [0:0] arg_0_output_z_stb, output [0:0] valid, input [31:0] arg_1, input [31:0] arg_2);
+
+
+	initial begin
+	end
+
+
+
+
+	fpu_add_inner inner(.arg_0_input_a(arg_0_input_a), .arg_0_input_a_ack(arg_0_input_a_ack), .arg_0_input_a_stb(arg_0_input_a_stb), .arg_0_input_b(arg_0_input_b), .arg_0_input_b_ack(arg_0_input_b_ack), .arg_0_input_b_stb(arg_0_input_b_stb), .arg_0_output_z(arg_0_output_z), .arg_0_output_z_stb(arg_0_output_z_stb), .arg_0_rst(arg_0_rst), .arg_1(arg_1), .arg_2(arg_2), .clk(clk), .rst(rst), .valid(valid));
+
+endmodule
