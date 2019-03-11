8c7699ca087bbb2ab7de36346d9d3e92c4cfb78e
Dillon Huff <dillonhuff@gmail.com>
2019-03-11 14:21:36 -0700

    Created eth tx interface

--- experiments/eth_axi_tx.cpp
+++ experiments/eth_axi_tx.cpp
@@ -18,24 +18,39 @@ public:
   void write_header(bit<48> dest_mac,
                     bit<48> src_mac,
                     bit<16> type) {
-    bit<1> is_ready = read_port(s_eth_hdr_ready);
-    stall(is_ready);
+  read_ready: bit<1> is_ready = read_port(s_eth_hdr_ready);
+  stall_on_ready: stall(is_ready);
 
-    write_port(s_eth_hdr_valid, 1);
-    write_port(s_eth_hdr_valid, 1);
-    write_port(s_eth_hdr_dest_mac, dest_mac);
-    write_port(s_eth_hdr_src_mac, src_mac);
-    write_port(s_eth_hdr_type, type);
+    add_constraint(start(read_ready) == start(stall_on_ready));
+
+  write_valid: write_port(s_eth_hdr_valid, 1);
+  write_src: write_port(s_eth_hdr_dest_mac, dest_mac);
+  write_dest: write_port(s_eth_hdr_src_mac, src_mac);
+  write_type: write_port(s_eth_hdr_type, type);
+
+    add_constraint(end(stall_on_ready) < start(write_valid));
+
+    add_constraint(start(write_valid) == start(write_src));
+    add_constraint(start(write_valid) == start(write_dest));
+    add_constraint(start(write_valid) == start(write_src));
+    add_constraint(start(write_valid) == start(write_type));
   }
 
   void write_bytes(bit<8> data,
                    bit<1> last) {
-    bit<1> is_ready = read_port(s_eth_hdr_ready);
-    stall(is_ready);
+  read_ready: bit<1> is_ready = read_port(s_eth_hdr_ready);
+  stall_on_ready: stall(is_ready);
 
-    write_port(s_eth_payload_tvalid, 1);
-    write_port(s_eth_payload_tlast, last);
-    write_port(s_eth_payload_tdata, data);        
+    add_constraint(start(read_ready) == start(stall_on_ready));    
+
+  write_valid: write_port(s_eth_payload_tvalid, 1);
+  write_last: write_port(s_eth_payload_tlast, last);
+  write_data: write_port(s_eth_payload_tdata, data);
+
+    add_constraint(end(stall_on_ready) < start(write_valid));
+
+    add_constraint(start(write_valid) == start(write_last));        
+    add_constraint(start(write_valid) == start(write_data));        
   }
 
 };
