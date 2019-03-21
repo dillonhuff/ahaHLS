52f1d8816ade0f56e73592e3687b9442c8e2ef88
Dillon Huff <dillonhuff@gmail.com>
2019-03-21 01:41:29 -0700

    Added verilator script

---
+++ experiments/run_verilator.sh
@@ -0,0 +1,8 @@
+# verilator --cc -Wno-fatal --top-module adder adder.v also an option?
+
+verilator --cc adder.v --exe use_fpu.cpp -Wno-lint
+
+make -j -C obj_dir -f Vadder.mk Vadder
+
+clang++ use_fpu.cpp ./obj_dir/verilated.o ./obj_dir/Vadder__ALL.a    -o Vadder_my -lm -lstdc++ -I/usr/local/Cellar/verilator/3.920/share/verilator/include
+
