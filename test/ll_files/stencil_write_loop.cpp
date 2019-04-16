cad94943dafc0bd274bc22351141f73ef0a80f77
Dillon Huff <dillonhuff@gmail.com>
2019-04-16 12:52:34 -0700

    Read and write loop working?

--- test/ll_files/stencil_write_loop.cpp
+++ test/ll_files/stencil_write_loop.cpp
@@ -12,11 +12,11 @@ void stencil_write_loop(hls_stream_AxiPackedStencil_uint16_t_1_1__ &arg_0,
 
         AxiPackedStencil_uint16_t_1_1_ _tmp_stencil;
         _tmp_stencil.copy(_hw_input_stencil_stream.read());
-        // if (_dim_0 >= 0 && _dim_0 <= 1 && _dim_1 >= 0 && _dim_1 <= 1)
-        //   {
+        if (_dim_1 >= 0 && _dim_0 <= 1 && _dim_1 >= 0 && _dim_1 <= 1)
+          {
         _mult_stencil_stream.write(_tmp_stencil);
             //_hw_input_stencil_stream_to_mult.write(_hw_input_stencil_stream.read());
-            //}
+          }
       }
   }
 }
