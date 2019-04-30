012306ca0e92c0ac4d89dfa5ead8eea9ba2ef39a
Dillon Huff <dillonhuff@gmail.com>
2019-04-30 13:41:44 -0700

    Do I want to handle pipeline to pipeline jumps any differently than ordinary state to state jumps? I suppose there could be hazards (in theory)

--- test/scheduling.cpp
+++ test/scheduling.cpp
@@ -1309,6 +1309,77 @@ namespace ahaHLS {
 
       REQUIRE(runIVerilogTB("task_parallel_loops"));
     }
+
+    SECTION("Each loop pipelined in a different task") {
+
+      ExecutionConstraints exec;
+      
+      inlineWireCalls(f, exec, interfaces);
+      addDataConstraints(f, exec);
+    
+      cout << "After inlining" << endl;
+      cout << valueString(f) << endl;
+
+      set<BasicBlock*> task0{entryBlk, loop0Blk};
+      set<BasicBlock*> task1{loop1Blk, exitBlk};
+      set<TaskSpec> tasks{{task0}, {task1}};
+      
+      auto preds = buildControlPreds(f);
+
+      set<PipelineSpec> toPipeline;
+      toPipeline.insert({false, {loop0Blk}});
+      toPipeline.insert({false, {loop1Blk}});
+      
+      SchedulingProblem p =
+        createSchedulingProblem(f, hcs, toPipeline, tasks, preds);
+      exec.addConstraints(p, f);
+
+      map<Function*, SchedulingProblem> constraints{{f, p}};
+      Schedule s = scheduleFunction(f, hcs, toPipeline, constraints);
+      STG graph = buildSTG(s, f);
+
+      cout << "STG Is" << endl;
+      graph.print(cout);
+
+      map<string, int> testLayout = {{"arg_0", 0}};
+      map<llvm::Value*, int> layout;
+      auto arch = buildMicroArchitecture(graph, layout, hcs);
+
+      VerilogDebugInfo info;
+      addNoXChecks(arch, info);
+
+      emitVerilog(arch, info);
+
+      // Create testing infrastructure
+      map<string, vector<int> > memoryInit{{"arg_0", {6}}};
+      map<string, vector<int> > memoryExpected{{"arg_0", {0, 1, 2, 3, 0, 1, 2, 3}}};
+
+      auto arg0 = dyn_cast<Argument>(getArg(f, 0));
+      string in0Name = string(arg0->getName());
+    
+      TestBenchSpec tb;
+      tb.memoryExpected = memoryExpected;
+      tb.name = "task_parallel_loops";
+      tb.useModSpecs = true;
+      int startSetMemCycle = 1;
+    
+      int startRunCycle = startSetMemCycle + 2; 
+      map_insert(tb.actionsInCycles, startRunCycle, string("rst_reg = 1;"));
+      map_insert(tb.actionsInCycles, startRunCycle + 1, string("rst_reg = 0;"));
+
+      int checkMemCycle = 100;
+      checkRAM(tb, checkMemCycle, "arg_0", memoryExpected, testLayout);
+
+      checkSignal(tb,
+                   "valid",
+                  {{3, 0}, {10, 0}, {15, 0}, {17, 0}, {19, 0}, {20, 1}, {25, 1}, {30, 1}, {35, 1}, {40, 1}, {100, 1}});
+
+      //tb.actionOnCondition("1", "$display(\"valid = %d\", valid);");
+      emitVerilogTestBench(tb, arch, testLayout);
+
+      REQUIRE(runIVerilogTB("task_parallel_loops"));
+    }
+    
     
   }
   
