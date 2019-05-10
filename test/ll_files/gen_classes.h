3cc08c7b95b72ca3bd219821dfde0758494e5527
Dillon Huff <dillonhuff@gmail.com>
2019-05-09 21:18:42 -0700

    Examining cascade outputs

--- test/ll_files/gen_classes.h
+++ test/ll_files/gen_classes.h
@@ -1,66 +1,237 @@
-#include <stdint.h>
+#include <cstdint>
 #include <cstdlib>
 
 class AxiPackedStencil_uint16_t_1_1_;
-
-class PackedStencil_uint16_t_1_1_;
-
-class Stencil_uint16_t_1_1_;
 
 class AxiPackedStencil_uint16_t_1_1_ {
 	uint16_t other_elems[1*1 + 20];
 	uint16_t elems[1*1];
   public:
-
+	void set_last(const int);
+	void set(const uint16_t c, const size_t e0=0, const size_t e1=0, const size_t e2=0);
+	uint16_t get(const size_t e0=0, const size_t e1=0, const size_t e2=0);
 	AxiPackedStencil_uint16_t_1_1_();
 	AxiPackedStencil_uint16_t_1_1_(const AxiPackedStencil_uint16_t_1_1_&);
-  //AxiPackedStencil_uint16_t_1_1_(const AxiPackedStencil_uint16_t_1_1_&&);
-  AxiPackedStencil_uint16_t_1_1_ operator=(const AxiPackedStencil_uint16_t_1_1_&);
-  
-	void set_last(const int);
-  //AxiPackedStencil_uint16_t_1_1_();
-  //uint16_t operator()(const size_t e0=0, const size_t e1=0, const size_t e2=0);
-	uint16_t get(const size_t e0=0, const size_t e1=0, const size_t e2=0);  
-	void set(uint16_t, const size_t e0=0, const size_t e1=0, const size_t e2=0);
-
-  void copy(const AxiPackedStencil_uint16_t_1_1_& in);
-	operator PackedStencil_uint16_t_1_1_();
-	operator Stencil_uint16_t_1_1_();
+	void copy(const AxiPackedStencil_uint16_t_1_1_& in);
+	AxiPackedStencil_uint16_t_1_1_ operator=(const AxiPackedStencil_uint16_t_1_1_&);
 };
 
-class PackedStencil_uint16_t_1_1_ {
-	uint16_t other_elems[1*1 + 20];
-	uint16_t elems[1*1];
+class AxiPackedStencil_int32_t_1_1_;
+
+class AxiPackedStencil_uint16_t_3_3_ {
+	uint16_t other_elems[3*3 + 20];
+	uint16_t elems[3*3];
   public:
-	PackedStencil_uint16_t_1_1_();
-	operator AxiPackedStencil_uint16_t_1_1_();
 	void set_last(const int);
-	uint16_t operator()(const size_t e0=0, const size_t e1=0, const size_t e2=0);
+	void set(const uint16_t c, const size_t e0=0, const size_t e1=0, const size_t e2=0);
+	uint16_t get(const size_t e0=0, const size_t e1=0, const size_t e2=0);
+	AxiPackedStencil_uint16_t_3_3_();
+	AxiPackedStencil_uint16_t_3_3_(const AxiPackedStencil_uint16_t_3_3_&);
+	void copy(const AxiPackedStencil_uint16_t_3_3_& in);
+	AxiPackedStencil_uint16_t_3_3_ operator=(const AxiPackedStencil_uint16_t_3_3_&);
 };
 
-class Stencil_uint16_t_1_1_ {
-	uint16_t other_elems[1*1 + 20];
-	uint16_t elems[1*1];
+class AxiPackedStencil_int32_t_1_1_ {
+	int32_t other_elems[1*1 + 20];
+	int32_t elems[1*1];
   public:
-	Stencil_uint16_t_1_1_();
-	Stencil_uint16_t_1_1_(const Stencil_uint16_t_1_1_&);
-	Stencil_uint16_t_1_1_(const Stencil_uint16_t_1_1_&&);
-	Stencil_uint16_t_1_1_& operator=(const Stencil_uint16_t_1_1_&);
 	void set_last(const int);
-	uint16_t operator()(const size_t e0=0, const size_t e1=0, const size_t e2=0);
-	void write(uint16_t, const size_t e0=0, const size_t e1=0, const size_t e2=0);
-	operator AxiPackedStencil_uint16_t_1_1_();
+	void set(const int32_t c, const size_t e0=0, const size_t e1=0, const size_t e2=0);
+	int32_t get(const size_t e0=0, const size_t e1=0, const size_t e2=0);
+	AxiPackedStencil_int32_t_1_1_();
+	AxiPackedStencil_int32_t_1_1_(const AxiPackedStencil_int32_t_1_1_&);
+	void copy(const AxiPackedStencil_int32_t_1_1_& in);
+	AxiPackedStencil_int32_t_1_1_ operator=(const AxiPackedStencil_int32_t_1_1_&);
 };
 
+class AxiPackedStencil_int32_t_3_3_ {
+	int32_t other_elems[3*3 + 20];
+	int32_t elems[3*3];
+  public:
+	void set_last(const int);
+	void set(const int32_t c, const size_t e0=0, const size_t e1=0, const size_t e2=0);
+	int32_t get(const size_t e0=0, const size_t e1=0, const size_t e2=0);
+	AxiPackedStencil_int32_t_3_3_();
+	AxiPackedStencil_int32_t_3_3_(const AxiPackedStencil_int32_t_3_3_&);
+	void copy(const AxiPackedStencil_int32_t_3_3_& in);
+	AxiPackedStencil_int32_t_3_3_ operator=(const AxiPackedStencil_int32_t_3_3_&);
+};
+
+class AxiPackedStencil_int16_t_1_1_;
+
+class AxiPackedStencil_int16_t_1_1_ {
+	int16_t other_elems[1*1 + 20];
+	int16_t elems[1*1];
+  public:
+	void set_last(const int);
+	void set(const int16_t c, const size_t e0=0, const size_t e1=0, const size_t e2=0);
+	int16_t get(const size_t e0=0, const size_t e1=0, const size_t e2=0);
+	AxiPackedStencil_int16_t_1_1_();
+	AxiPackedStencil_int16_t_1_1_(const AxiPackedStencil_int16_t_1_1_&);
+	void copy(const AxiPackedStencil_int16_t_1_1_& in);
+	AxiPackedStencil_int16_t_1_1_ operator=(const AxiPackedStencil_int16_t_1_1_&);
+};
+
+class AxiPackedStencil_int16_t_3_3_ {
+	int16_t other_elems[3*3 + 20];
+	int16_t elems[3*3];
+  public:
+	void set_last(const int);
+	void set(const int16_t c, const size_t e0=0, const size_t e1=0, const size_t e2=0);
+	int16_t get(const size_t e0=0, const size_t e1=0, const size_t e2=0);
+	AxiPackedStencil_int16_t_3_3_();
+	AxiPackedStencil_int16_t_3_3_(const AxiPackedStencil_int16_t_3_3_&);
+	void copy(const AxiPackedStencil_int16_t_3_3_& in);
+	AxiPackedStencil_int16_t_3_3_ operator=(const AxiPackedStencil_int16_t_3_3_&);
+};
+
+typedef AxiPackedStencil_uint16_t_1_1_ PackedStencil_uint16_t_1_1_;
+typedef AxiPackedStencil_uint16_t_1_1_ Stencil_uint16_t_1_1_;
+typedef AxiPackedStencil_int32_t_1_1_ PackedStencil_int32_t_1_1_;
+typedef AxiPackedStencil_int32_t_1_1_ Stencil_int32_t_1_1_;
+typedef AxiPackedStencil_int32_t_3_3_ Stencil_int32_t_3_3_;
+typedef AxiPackedStencil_int32_t_3_3_ PackedStencil_int32_t_3_3_;
+typedef AxiPackedStencil_uint16_t_3_3_ PackedStencil_uint16_t_3_3_;
+typedef AxiPackedStencil_uint16_t_3_3_ Stencil_uint16_t_3_3_;
+typedef AxiPackedStencil_int16_t_1_1_ PackedStencil_int16_t_1_1_;
+typedef AxiPackedStencil_int16_t_1_1_ Stencil_int16_t_1_1_;
+typedef AxiPackedStencil_int16_t_1_1_ PackedStencil_int16_t_3_3_;
+typedef AxiPackedStencil_int16_t_1_1_ Stencil_int16_t_3_3_;
 class hls_stream_AxiPackedStencil_uint16_t_1_1__ {
 	int index;
 	AxiPackedStencil_uint16_t_1_1_ elems[1000];
   public:
 	hls_stream_AxiPackedStencil_uint16_t_1_1__();
-	hls_stream_AxiPackedStencil_uint16_t_1_1__(const hls_stream_AxiPackedStencil_uint16_t_1_1__&);
-  
 	AxiPackedStencil_uint16_t_1_1_ read();
 	void write(AxiPackedStencil_uint16_t_1_1_);
 };
 
+class hls_stream_AxiPackedStencil_int16_t_1_1__ {
+	int index;
+	AxiPackedStencil_int16_t_1_1_ elems[1000];
+  public:
+	hls_stream_AxiPackedStencil_int16_t_1_1__();
+	AxiPackedStencil_int16_t_1_1_ read();
+	void write(AxiPackedStencil_int16_t_1_1_);
+};
+
+class hls_stream_AxiPackedStencil_uint16_t_3_3__ {
+	int index;
+	AxiPackedStencil_uint16_t_3_3_ elems[1000];
+  public:
+	hls_stream_AxiPackedStencil_uint16_t_3_3__();
+	AxiPackedStencil_uint16_t_3_3_ read();
+	void write(AxiPackedStencil_uint16_t_3_3_);
+};
+
+class hls_stream_AxiPackedStencil_int16_t_3_3__ {
+	int index;
+	AxiPackedStencil_int16_t_3_3_ elems[1000];
+  public:
+	hls_stream_AxiPackedStencil_int16_t_3_3__();
+	AxiPackedStencil_int16_t_3_3_ read();
+	void write(AxiPackedStencil_int16_t_3_3_);
+};
+
+class hls_stream_AxiPackedStencil_int32_t_1_1__ {
+	int index;
+	AxiPackedStencil_int32_t_1_1_ elems[1000];
+  public:
+	hls_stream_AxiPackedStencil_int32_t_1_1__();
+	AxiPackedStencil_int32_t_1_1_ read();
+	void write(AxiPackedStencil_int32_t_1_1_);
+};
+
+class hls_stream_AxiPackedStencil_int32_t_3_3__ {
+	int index;
+	AxiPackedStencil_int32_t_3_3_ elems[1000];
+  public:
+	hls_stream_AxiPackedStencil_int32_t_3_3__();
+	AxiPackedStencil_int32_t_3_3_ read();
+	void write(AxiPackedStencil_int32_t_3_3_);
+};
+
+class hls_stream_PackedStencil_uint16_t_1_1__ {
+	int index;
+	PackedStencil_uint16_t_1_1_ elems[1000];
+  public:
+	hls_stream_PackedStencil_uint16_t_1_1__();
+	PackedStencil_uint16_t_1_1_ read();
+	void write(PackedStencil_uint16_t_1_1_);
+};
+
+class hls_stream_PackedStencil_int16_t_1_1__ {
+	int index;
+	PackedStencil_int16_t_1_1_ elems[1000];
+  public:
+	hls_stream_PackedStencil_int16_t_1_1__();
+	PackedStencil_int16_t_1_1_ read();
+	void write(PackedStencil_int16_t_1_1_);
+};
+
+class hls_stream_PackedStencil_uint16_t_3_3__ {
+	int index;
+	PackedStencil_uint16_t_3_3_ elems[1000];
+  public:
+	hls_stream_PackedStencil_uint16_t_3_3__();
+	PackedStencil_uint16_t_3_3_ read();
+	void write(PackedStencil_uint16_t_3_3_);
+};
+
+class hls_stream_PackedStencil_int16_t_3_3__ {
+	int index;
+	PackedStencil_int16_t_3_3_ elems[1000];
+  public:
+	hls_stream_PackedStencil_int16_t_3_3__();
+	PackedStencil_int16_t_3_3_ read();
+	void write(PackedStencil_int16_t_3_3_);
+};
+
+class hls_stream_PackedStencil_int32_t_1_1__ {
+	int index;
+	PackedStencil_int32_t_1_1_ elems[1000];
+  public:
+	hls_stream_PackedStencil_int32_t_1_1__();
+	PackedStencil_int32_t_1_1_ read();
+	void write(PackedStencil_int32_t_1_1_);
+};
+
+class hls_stream_PackedStencil_int32_t_3_3__ {
+	int index;
+	PackedStencil_int32_t_3_3_ elems[1000];
+  public:
+	hls_stream_PackedStencil_int32_t_3_3__();
+	PackedStencil_int32_t_3_3_ read();
+	void write(PackedStencil_int32_t_3_3_);
+};
+
+class linebuffer_hls_stream_PackedStencil_int32_t_1_1___to_hls_stream_PackedStencil_int32_t_3_3___bnds_62_62 {
+public:
+PackedStencil_int32_t_3_3_ lb_read();
+void lb_write(const PackedStencil_int32_t_1_1_& stencil);
+bool has_valid_data();
+};
+class linebuffer_hls_stream_PackedStencil_int32_t_1_1___to_hls_stream_PackedStencil_int32_t_3_3___bnds_14_14 {
+public:
+PackedStencil_int32_t_3_3_ lb_read();
+void lb_write(const PackedStencil_int32_t_1_1_& stencil);
+bool has_valid_data();
+};
+class linebuffer_hls_stream_AxiPackedStencil_uint16_t_1_1___to_hls_stream_PackedStencil_uint16_t_3_3___bnds_64_64 {
+public:
+PackedStencil_uint16_t_3_3_ lb_read();
+void lb_write(const AxiPackedStencil_uint16_t_1_1_& stencil);
+bool has_valid_data();
+};
+class linebuffer_hls_stream_AxiPackedStencil_uint16_t_1_1___to_hls_stream_PackedStencil_uint16_t_3_3___bnds_16_16 {
+public:
+PackedStencil_uint16_t_3_3_ lb_read();
+void lb_write(const AxiPackedStencil_uint16_t_1_1_& stencil);
+bool has_valid_data();
+};
+class ram_int32_t_9 {
+public:
+int32_t ram_read(const int addr);
+void ram_write(const int addr, int32_t value);
+};
 
