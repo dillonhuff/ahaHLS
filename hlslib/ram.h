3f9c2e5985c11c849b6ec898a25f2425bd9ed13d
Dillon Huff <dillonhuff@gmail.com>
2019-07-13 15:38:05 -0700

    Playing with stdlib definitions

--- hlslib/ram.h
+++ hlslib/ram.h
@@ -1,14 +1,15 @@
 #pragma once
 
-#define AHAHLS_SYNTH_BUILTIN   __attribute__((optnone,noinline))
+#define AHAHLS_SYNTH_BUILTIN __attribute__((optnone,noinline))
 
 namespace ahaHLSLib {
   
-  template<typename DataType, typename AddressType, int depth>
+  template<typename DataType, typename AddressType, int depth, int numReadPorts, int numWritePorts>
   class ram {
 
   public:
-    
+
+    AHAHLS_SYNTH_BUILTIN
     DataType read(const AddressType addr) {
       return 0;
     }
