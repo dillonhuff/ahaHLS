fde7271b3238ade4399cd106b378c8017db082e0
Dillon Huff <dillonhuff@gmail.com>
2019-07-18 21:56:11 -0700

    Creating initial block set

--- test/dependence.cpp
+++ test/dependence.cpp
@@ -8,10 +8,7 @@
 
 #include <fstream>
 
-#include <llvm/IR/LegacyPassManager.h>
-#include <llvm/Transforms/IPO/PassManagerBuilder.h>
-#include <llvm/Analysis/AliasAnalysis.h>
-#include <llvm/Analysis/ScalarEvolution.h>
+#include <llvm/IR/Dominators.h>
 #include <llvm/Analysis/LoopInfo.h>
 
 using namespace llvm;
@@ -19,11 +16,65 @@ using namespace std;
 
 namespace ahaHLS {
 
+  class SymFrame {
+  public:
+    BasicBlock* lastBlock;
+  };
+
+  class SymTrace {
+  public:
+    
+    std::deque<SymFrame*> active;
+
+    void setStartBlock(BasicBlock* blk) {
+      for (auto pred : predecessors(blk)) {
+        auto frame = new SymFrame();
+        frame->lastBlock = pred;
+        active.push_back(frame);
+      }
+    }
+
+    ~SymTrace() {
+      for (auto frame : active) {
+        delete frame;
+      }
+    }
+  };
+  
+  class SymExe {
+
+  public:
+
+    llvm::Function* f;
+
+    SymExe(llvm::Function* f_) : f(f_) {}
+
+    void analyzeLoops() {
+      DominatorTree dt(*f);
+      LoopInfo li(dt);
+
+      for (Loop* loop : li) {
+        cout << "Analyzing loop with " << loop->getBlocks().size() << " basic block(s)" << endl;
+
+        BasicBlock* h = loop->getHeader();
+        assert(h != nullptr);
+
+        cout << valueString(h) << endl;
+
+        SymTrace trace;
+        trace.setStartBlock(h);
+
+        cout << "# of active blocks " << trace.active.size() << endl;
+      }
+    }
+  };
+  
   TEST_CASE("Symbolic analysis of histogram") {
     LLVMContext context;
     setGlobalLLVMContext(&context);
 
     auto mod = llvm::make_unique<Module>("Unroll by two", context);
+    setGlobalLLVMModule(mod.get());
 
     std::vector<Type *> inputs{sramType(32, 16)->getPointerTo(),
         sramType(32, 16)->getPointerTo()};
@@ -46,10 +97,10 @@ namespace ahaHLS {
     IRBuilder<> loopBuilder(loopBlock);
     auto indPhi = loopBuilder.CreatePHI(intType(32), 2);
     auto pix = loadRAMVal(loopBuilder, imgRam, indPhi);
-    //auto sumPhi = loopBuilder.CreatePHI(intType(32), 2);
+    auto count = loadRAMVal(loopBuilder, histRam, pix);
+    auto nextCount = loopBuilder.CreateAdd(count, one);
+    storeRAMVal(loopBuilder, histRam, pix, nextCount);
     auto nextInd = loopBuilder.CreateAdd(indPhi, one);
-    //auto nextSum = loopBuilder.CreateAdd(sumPhi, ldA);
-
     auto exitCond = loopBuilder.CreateICmpNE(nextInd, loopBound);
 
     indPhi->addIncoming(zero, entryBlock);
@@ -60,12 +111,15 @@ namespace ahaHLS {
     IRBuilder<> exitBuilder(exitBlock);
     exitBuilder.CreateRet(nullptr);
 
+    cout << "LLVM Function" << endl;
     cout << valueString(f) << endl;
 
-    HardwareConstraints hcs = standardConstraints();
-
-    scheduleFunction(f, hcs);
+    SymExe engine(f);
+    engine.analyzeLoops();
     
+    // HardwareConstraints hcs = standardConstraints();
+
+    // scheduleFunction(f, hcs);
   }
 
   // TEST_CASE("Computing dependence distances via loop vectorizer") {
