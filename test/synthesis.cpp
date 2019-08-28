3117780db8f5d8390061f90abee979eceff1191a
Dillon Huff <dillonhuff@gmail.com>
2019-08-27 23:11:30 -0700

    Filling in a memory test

--- test/synthesis.cpp
+++ test/synthesis.cpp
@@ -27,11 +27,11 @@ namespace ahaHLS {
     std::vector<Type *> inputs{sramType(32, 512)->getPointerTo(),
         sramType(32, 512)->getPointerTo(),
         sramType(32, 512)->getPointerTo()};
-    Function* srUser = mkFunc(inputs, "accum_loop", mod.get());
+    Function* f = mkFunc(inputs, "vadd", mod.get());
 
-    auto entryBlock = mkBB("entry_block", srUser);
-    auto loopBlock = mkBB("loop_block", srUser);
-    auto exitBlock = mkBB("exit_block", srUser);        
+    auto entryBlock = mkBB("entry_block", f);
+    auto loopBlock = mkBB("loop_block", f);
+    auto exitBlock = mkBB("exit_block", f);        
 
     IRBuilder<> entryBuilder(entryBlock);
     
@@ -46,9 +46,67 @@ namespace ahaHLS {
     auto nextInd = loopBuilder.CreateAdd(indPhi, one);
 
     auto exitCond = loopBuilder.CreateICmpNE(nextInd, loopBound);
-
+    loopBuilder.CreateCondBr(exitCond, exitBlock, loopBlock);
     indPhi->addIncoming(zero, entryBlock);
     indPhi->addIncoming(nextInd, loopBlock);
+
+    IRBuilder<> exitBuilder(exitBlock);
+    exitBuilder.CreateRet(nullptr);
+
+    // Build verilog
+
+    HardwareConstraints hcs = standardConstraints();
+    hcs.typeSpecs[string("SRAM_32_16")] =
+      [](StructType* tp) { return ramSpec(32, 16); };
+    InterfaceFunctions interfaces;
+    Function* ramRead = ramLoadFunction(getArg(f, 0));
+    interfaces.addFunction(ramRead);
+    implementRAMRead0(ramRead,
+                      interfaces.getConstraints(ramRead));
+
+    Function* ramWrite = ramStoreFunction(getArg(f, 1));
+    interfaces.addFunction(ramWrite);
+    implementRAMWrite0(ramWrite,
+                       interfaces.getConstraints(ramWrite));
+
+    ExecutionConstraints exec;
+    set<BasicBlock*> toPipeline;
+    Schedule s = scheduleInterface(f, hcs, interfaces, toPipeline, exec);
+    STG graph = buildSTG(s, f);
+
+    cout << "STG Is" << endl;
+    graph.print(cout);
+
+    map<llvm::Value*, int> layout;
+    auto arch = buildMicroArchitecture(graph, layout, hcs);
+
+    VerilogDebugInfo info;
+    addNoXChecks(arch, info);
+
+    emitVerilog(arch, info);
+    TestBenchSpec tb;
+    tb.runCycles = 600;
+    tb.name = "vadd";
+    tb.useModSpecs = true;
+    
+    map<string, int> testLayout = {{"arg_0", 0}, {"arg_1", 0}};
+    
+    map<string, vector<int> > memoryInit{{"arg_0", {6}}};
+    map<string, vector<int> > memoryExpected{{"arg_1", {11}}};
+    
+    int startSetMemCycle = 1;
+    setRAM(tb, 1, "arg_0", memoryInit, testLayout);
+
+    int startRunCycle = startSetMemCycle + 10; 
+    map_insert(tb.actionsInCycles, startRunCycle, string("rst_reg = 1;"));
+    map_insert(tb.actionsInCycles, startRunCycle + 1, string("rst_reg = 0;"));
+
+    int checkMemCycle = 30;
+    checkRAM(tb, checkMemCycle, "arg_1", memoryExpected, testLayout);
+
+    emitVerilogTestBench(tb, arch, testLayout);
+
+    REQUIRE(runIVerilogTB("vadd"));
   }
 
   TEST_CASE("Vector add fifos") {
